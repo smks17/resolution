bf301539e2f7ee7df4b662eb122e62d2591dbc47
mahdi <esmokes17@gmail.com>
2021-12-14 18:19:25 +0330

    write prove function

--- resolution.py
+++ resolution.py
@@ -1,4 +1,4 @@
-from enum import Enum
+from enum import Enum, Flag
 from typing import Union
 
 __all__ = ['Operation', 'Cluese', 'parse', 'prove']
@@ -58,107 +58,178 @@ class Operation(Enum):
         else:
             return ''   
 
-class CluaseSentence:
-    cluase: Union[tuple, str]
+class ClauseSentence:
+    clause: Union[tuple, str]
     operand: Operation
-    def __init__(self, operand: Operation, cluase: Union[tuple, str]):
+    def __init__(self, operand: Operation, clause: tuple):
         if(operand == Operation.NONE):
-            assert isinstance(cluase, str), "you should use str or tuple with Cluse type element"
-        elif(operand == Operation.NEGATION):
-            assert isinstance(cluase, CluaseSentence), "you should use str or tuple with Cluse type element"
+            assert isinstance(clause, str), "you should use str or tuple with Cluse type element"
+        # elif(operand == Operation.NEGATION):
+        #     assert isinstance(clause, ClauseSentence), "you should use str or tuple with Cluse type element"
         else:
-            assert all(isinstance(i, CluaseSentence) for i in cluase), "you should use str or tuple with Cluse type element"
+            assert all(isinstance(i, ClauseSentence) for i in clause), "you should use str or tuple with Cluse type element"
         self.operand = operand
-        self.cluase = cluase
+        self.clause = clause
+
+    def toString(self):
+        if(self.operand == Operation.NONE):
+            return self.clause
+        elif(self.operand == Operation.NEGATION):
+            return '~'+self.clause[0].clause
     
     def __key(self):
-        return(self.operand, self.cluase)
+        return(self.operand, self.clause)
     
     def __hash__(self) -> int:
         return hash(self.__key())
 
     def __eq__(self, other) -> bool:
-        return (isinstance(other, type(self)) and (self.cluase, self.operand) == (other.cluase, other.operand))        
+        return (isinstance(other, type(self)) and (self.clause, self.operand) == (other.clause, other.operand))        
 
     def __str__(self):
         if(self.operand == Operation.NONE or self.operand == Operation.NEGATION):
             op = self.operand.symbol
-            return f'{op} {str(self.cluase.__str__())}'
+            return f'{op} {str(self.clause.__str__())}'
         op = self.operand.symbol
-        return f'{self.cluase[0].__str__()} {op} {self.cluase[1].__str__()}'
+        return f'{self.clause[0].__str__()} {op} {self.clause[1].__str__()}'
 
-def convert(term: CluaseSentence):
-    """ convert implication and equavalnce to disjunction """
+def convert(term: ClauseSentence):
+    """ convert implication and equavalnce to disjunction or conjunction"""
+    isNegation = False
+    if(term.operand == Operation.NEGATION):
+        term = term.clause
+        isNegation = True
     if(term.operand == Operation.IMPLICATION):
-        cluase = ( (Operation.NEGATION, (term.cluase[0],) ), term.cluase[1] )
-        return ( Operation.DISJUNCTION, cluase ) 
+        clause = (ClauseSentence(Operation.NEGATION, (term.clause[0],) ), term.clause[1])
+        res = ClauseSentence( Operation.CONJUNCTION, clause )
+        if(isNegation):
+            return applyNegation(ClauseSentence( Operation.NEGATION, (res,)))
+        return res
     elif(term.operand == Operation.EQUVALENCE):
-        cluase1 = ( Operation.CONJUNCTION, ((Operation.NEGATION, term.cluase[0],), term.cluase[1]))
-        cluase2 = ( Operation.CONJUNCTION, (term.cluase[0], (Operation.NEGATION, term.cluase[1],)))
-        return (Operation.DISJUNCTION, (cluase1, cluase2))
+        clause1 = ClauseSentence( Operation.CONJUNCTION, ClauseSentence(ClauseSentence(Operation.NEGATION, term.clause[0],), term.clause[1]))
+        clause2 = ClauseSentence( Operation.CONJUNCTION, ClauseSentence(term.clause[0], ClauseSentence(Operation.NEGATION, term.clause[1],)))
+        res = ClauseSentence(Operation.DISJUNCTION, (clause1, clause2))
+        if(isNegation):
+            return applyNegation(ClauseSentence( Operation.NEGATION, (res,)))
+        return res
 
-def simplificationNegation(term: CluaseSentence):
-    while(term.operand == Operation.NEGATION and term.cluase.operand == Operation.NEGATION):
-        term = term.cluasecluasee
+def simplificationNegation(term: ClauseSentence):
+    while(term.operand == Operation.NEGATION and term.clause[0].operand == Operation.NEGATION):
+        term = term.clauseclausee
     return term
 
-def distribution(term: CluaseSentence):
-    if(
-        not term.cluase[0].operand == Operation.NONE
-        and term.cluase[1].operand == Operation.NONE
-        and not term.cluase[0].operand == term.operand
+def distribution(term: ClauseSentence):
+    if(term.operand == Operation.NEGATION):
+        return None
+    elif(
+        not term.clause[0].operand == Operation.NONE
+        and term.clause[1].operand == Operation.NONE
+        and not term.clause[0].operand == term.operand
+        and term.clause[0].operand in [Operation.DISJUNCTION, Operation.CONJUNCTION]
     ):
-        cluase1 = CluaseSentence(term.operand, (term.cluase[0].cluase[0], term.cluase[1]))
-        cluase2 = CluaseSentence(term.operand, (term.cluase[0].cluase[1], term.cluase[1]))
-        return CluaseSentence(term.cluase[0].operand, (cluase1, cluase2))
+        clause1 = ClauseSentence(term.operand, (term.clause[0].clause[0], term.clause[1]))
+        clause2 = ClauseSentence(term.operand, (term.clause[0].clause[1], term.clause[1]))
+        return ClauseSentence(term.clause[0].operand, (clause1, clause2))
 
     elif(
-        not term.cluase[1].operand == Operation.NONE
-        and term.cluase[0].operand == Operation.NONE
-        and not term.cluase[1].operand == term.operand
+        not term.clause[1].operand == Operation.NONE
+        and term.clause[0].operand == Operation.NONE
+        and not term.clause[1].operand == term.operand
+        and term.clause[1].operand in [Operation.DISJUNCTION, Operation.CONJUNCTION]
     ):
-        cluase1 = CluaseSentence(term.operand, (term.cluase[1].cluase[0], term.cluase[0]))
-        cluase2 = CluaseSentence(term.operand, (term.cluase[1].cluase[0], term.cluase[0]))
-        return CluaseSentence(term.cluase[0].operand, (cluase1, cluase2))
+        clause1 = ClauseSentence(term.operand, (term.clause[1].clause[0], term.clause[0]))
+        clause2 = ClauseSentence(term.operand, (term.clause[1].clause[0], term.clause[0]))
+        return ClauseSentence(term.clause[0].operand, (clause1, clause2))
 
-def splitByDisjunction(term: CluaseSentence):
+def splitByDisjunction(term: ClauseSentence):
     result = set()
     if(term.operand == Operation.DISJUNCTION):
-        cluase1 = term.cluase[0]
-        cluase2 = term.cluase[1]
-        if(not cluase1.operand == Operation.NONE and not cluase1.operand == Operation.NEGATION):
-            result |= (splitByDisjunction(cluase1))
+        clause1 = term.clause[0]
+        clause2 = term.clause[1]
+        if(not clause1.operand == Operation.NONE and not clause1.operand == Operation.NEGATION):
+            result |= (splitByDisjunction(clause1))
         else:
-            result.add(cluase1)
-        if(not cluase2.operand == Operation.NONE and not cluase2.operand == Operation.NEGATION):
-            result |= (splitByDisjunction(cluase2))
+            result.add(clause1)
+        if(not clause2.operand == Operation.NONE and not clause2.operand == Operation.NEGATION):
+            result |= (splitByDisjunction(clause2))
         else:
-            result.add(cluase2)
+            result.add(clause2)
         return result
     elif(term.operand == Operation.CONJUNCTION):
-        return set([term.cluase])         
+        return set([term.clause[0].toString(), term.clause[1].toString()])
+    else:
+        return set([term.toString()])
 
-def prove(cluases: list, coclusion):
-    """
-        if cluases can prove conlusion return True otherwise False
-    """
-    pass
-        
+def subscription(premise1: set, premise2: set):
+    for pre1 in premise1:
+        for pre2 in premise2:
+            if(pre1[-1] == pre2[-1]):
+                temp = premise1.copy()
+                temp.remove(pre1)
+                temp |= premise2.copy()
+                temp = temp
+                temp.remove(pre2)
+                yield (temp)
 
+def applyNegation(term: ClauseSentence):
+    if(not term.operand == Operation.NEGATION):
+        return term
+    elif(term.operand == Operation.CONJUNCTION):
+        cluase1 = simplificationNegation(Operation.NEGATION, (term.clause[0],))
+        cluase2 = simplificationNegation((Operation.NEGATION, (term.clause[1],)))
+        return ClauseSentence( Operation.DISJUNCTION, (cluase1 , cluase2) )
+    elif(term.operand == Operation.DISJUNCTION):
+        cluase1 = simplificationNegation(Operation.NEGATION, (term.clause[0],))
+        cluase2 = simplificationNegation((Operation.NEGATION, (term.clause[1],)))
+        return ClauseSentence( Operation.CONJUNCTION, (cluase1 , cluase2) )
 
-def parse(input: list) -> CluaseSentence:
+def isProvable(premises: list):
     """
-        parse a logic term to a tuple of operation and cluases
+        if clauses can prove conlusion return True otherwise False
+    """
+    for pre in premises:
+        for other in premises:
+            if(other != pre):
+                new = subscription(pre, other)
+                for x in new:
+                    if(x == None):
+                        continue
+                    elif(len(x) == 0):
+                        return True
+                    elif(not x in premises):
+                        premises.append(x)
+    return False
+
+def prove(premises: list[str], conclusion: list[str]):
+    tempPremises = premises.copy()
+    tempPremises.append('( ~ ' + conclusion + ' )')
+    premisesClause = []
+    for pre in tempPremises:
+        clause = parse(pre.split())
+        if(not clause.operand == Operation.NONE):
+            if(clause.operand == Operation.NEGATION):
+                clause = simplificationNegation(clause)
+            if(clause.operand == Operation.IMPLICATION or clause.operand == Operation.EQUVALENCE):
+                clause = convert(clause)
+            res = distribution(clause)
+            if(not res == None):
+                clause = res
+        premisesClause.append(splitByDisjunction(clause))
+    return isProvable(premisesClause)
+
+def parse(input: list) -> ClauseSentence:
+    """
+        parse a logic term to a ClauseSentence
 
         Parameter
         ---------
         input: list 
-            operation and cluases of a term
+            operation and clauses of a term
 
         Return
         ------
-        CluaseSentence
-            operation and cluase in form a CluaseSentence object 
+        ClauseSentence
+            operation and clause inform a ClauseSentence object 
         Examples
         -------
             >>> rawInput = 'p -> q'
@@ -171,7 +242,7 @@ def parse(input: list) -> CluaseSentence:
             (<Operation.DISJUNCTION: '/\'>, ((<Operation.NEGATION: '~'>, ((<Operation.NONE: ' '>, 'p'),)), (<Operation.EQUVALENCE: '<->'>, ((<Operation.NONE: ' '>, 'q'), (<Operation.NONE: ' '>, 'p')))))
     """
     if(len(input) == 1):
-        return CluaseSentence( Operation.NONE, input[0] )
+        return ClauseSentence( Operation.NONE, input[0] )
     i = 0
     while (i != len(input)):
         if(input[i] == '('):
@@ -191,9 +262,13 @@ def parse(input: list) -> CluaseSentence:
                 return parse(input[begin:i])
 
         if(Operation.getOperation(input[0]) == Operation.NEGATION):
-            cluase = parse(input[i+1:],)
-            return CluaseSentence( Operation.getOperation(input[i]), cluase )
+            clause = parse(input[i+1:])
+            return ClauseSentence( Operation.getOperation(input[i]), (clause,) )
         if(Operation.getOperation(input[i]) != Operation.NONE):
-            cluase = ( parse(input[0:i]), parse(input[i+1:len(input)]) )
-            return CluaseSentence( Operation.getOperation(input[i]) , cluase )
+            clause = ( parse(input[0:i]), parse(input[i+1:len(input)]) )
+            return ClauseSentence( Operation.getOperation(input[i]) , clause )
         i+=1
+
+if __name__ == "__main__":
+    print("you run from wrong place!")
+    print("please run with import this file in another file.")
